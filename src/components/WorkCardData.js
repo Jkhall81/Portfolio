cc7c76485a0e9e055058efcd10819d5ba7177f26
Jason K Hall <106277735+Jkhall81@users.noreply.github.com>
2022-07-19 19:40:49 -0700

    Update WorkCardData.js

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -7,19 +7,22 @@ const ProjectCardData = [
         imgsrc: pro1,
         title: "Netflix Clone",
         text: "A Netflix Clone created with React.",
-        view: "https://melodious-lollipop-6e6ded.netlify.app/"
+        view: "https://melodious-lollipop-6e6ded.netlify.app/",
+        source: "https://github.com/Jkhall81/Netflix"
     },
     {
         imgsrc: pro2,
         title: "Gmail Clone",
         text: "Project not yet completed",
-        view: "url.com"
+        view: "url.com",
+        source: "url.com"
     },
     {
         imgsrc: pro3,
         title: "Amazon Clone",
         text: "Project not yet completed",
-        view: "url.com"
+        view: "url.com",
+        source: "url.com"
     },
 ];
 
