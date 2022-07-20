95d7558a5c870cd453afc64d2ad6565fe93810c8
Jason K Hall <106277735+Jkhall81@users.noreply.github.com>
2022-07-19 20:24:40 -0700

    Add files via upload

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -3,27 +3,27 @@ import pro2 from "../assets/Project2.webp";
 import pro3 from "../assets/Project3.jpg";
 
 const ProjectCardData = [
-    {
-        imgsrc: pro1,
-        title: "Netflix Clone",
-        text: "A Netflix Clone created with React.",
-        view: "https://melodious-lollipop-6e6ded.netlify.app/",
-        source: "https://github.com/Jkhall81/Netflix"
-    },
-    {
-        imgsrc: pro2,
-        title: "Gmail Clone",
-        text: "Project not yet completed",
-        view: "url.com",
-        source: "url.com"
-    },
-    {
-        imgsrc: pro3,
-        title: "Amazon Clone",
-        text: "Project not yet completed",
-        view: "url.com",
-        source: "url.com"
-    },
+  {
+    imgsrc: pro1,
+    title: "Netflix Clone",
+    text: "A Netflix Clone created with React.",
+    view: "https://melodious-lollipop-6e6ded.netlify.app/",
+    source: "https://github.com/Jkhall81/Netflix",
+  },
+  {
+    imgsrc: pro2,
+    title: "Gmail Clone",
+    text: "Project not yet completed",
+    view: "url.com",
+    source: "url.com",
+  },
+  {
+    imgsrc: pro3,
+    title: "Amazon Clone",
+    text: "Project not yet completed",
+    view: "url.com",
+    source: "url.com",
+  },
 ];
 
 export default ProjectCardData;
